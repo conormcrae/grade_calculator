ea4664b381b35a91309f0d81f86f875942222a88
Conor McRae <comc@pic.ucla.edu>
2017-03-06 16:27:04 -0800

    Fiddled with widget.ui using Qt Design
    I used Qt's design mode to edit a visible window. I got eight sliders and spin boxes set in the window but have been having trouble getting the layout to look right.
    I was able to connect one pair of slider/spin box. I'm waiting to get the layout to work better to set the rest. Then I will add buttons, etc for the exams.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QPushButton>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    //MainWindow w;
+    //w.show();
+
+    QPushButton button ("Hello, world!");
+    button.setText("some text");
+    button.setToolTip("A hint...");
+
+
+    QPushButton button1("hi.", &button);
+    button.show();
 
     return a.exec();
 }
